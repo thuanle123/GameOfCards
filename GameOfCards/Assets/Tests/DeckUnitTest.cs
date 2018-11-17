a5aeb0209d9bc4c7ff0781080849786bef36d0c3
Thanut Art Parkeenvincha <art.parkeenvincha@gmail.com>
2018-11-17 15:10:47 -0800

    CardStackFunctions interface implemented for Unit Testing

--- GameOfCards/Assets/Tests/DeckUnitTest.cs
+++ GameOfCards/Assets/Tests/DeckUnitTest.cs
@@ -1,6 +1,4 @@
 ﻿using NUnit.Framework;
-using NSubstitute;
-using System.Collections.Generic;
 
 /*  ================= Unit test for CardStack.cs =================
  *  -Uses NSubstitute library: https://github.com/nsubstitute/nsubstitute
@@ -9,20 +7,20 @@ using System.Collections.Generic;
  *      -How to use NSubstitute in Unity: https://www.youtube.com/watch?v=xSa2S-W7x48
  */
 
-public class DeckUnitTest {
-
+public class DeckUnitTest
+{
     // TESTS:
     [Test]
     public void test_a_empty_deck_created()  // Check if empty deck with 0 cards can be created
     {
-        var deck = GetCardStackMock();
+        var deck = CardStackFunctions.GetCardStackMock();
         Assert.That(deck.CardCount, Is.EqualTo(0));
     }
 
     [Test]
     public void test_b_deck_holds_one_card()  // Check if it can store 1 card
     {
-        var deck = GetCardStackMock();
+        var deck = CardStackFunctions.GetCardStackMock();
         deck.push(0);
         Assert.That(deck.CardCount, Is.EqualTo(1));
     }
@@ -30,7 +28,7 @@ public class DeckUnitTest {
     [Test]
     public void test_c_deck_holds_draws_card_correctly()   // Check if it can hold and draw 1 random card
     {
-        var deck = GetCardStackMock();
+        var deck = CardStackFunctions.GetCardStackMock();
         System.Random r = new System.Random();  // Create a random number generator
         var randomCard = r.Next(0, 52);         // Generate a random card between 0 to 51
         deck.push(randomCard);                  // Push it onto the deck
@@ -40,24 +38,24 @@ public class DeckUnitTest {
     [Test]
     public void test_d_deck_holds_52_cards()   // Check if it can hold a maximum of 52 cards
     {
-        var deck = GetCardStackMock();
-        FillWithCards(deck, 52);    // Fill it with 52 cards
+        var deck = CardStackFunctions.GetCardStackMock();
+        CardStackFunctions.FillWithCards(deck, 52);    // Fill it with 52 cards
         Assert.That(deck.CardCount, Is.EqualTo(52));
     }
 
     [Test]
     public void test_e_is_shuffled() // Checks to see if cards are shuffled
-    { 
+    {
 
         //Creates 2 decks old and new, will be the same cards one will be shuffled and one will not
 
         // This will not change 
-        var unshuffledDeck = GetCardStackMock();
+        var unshuffledDeck = CardStackFunctions.GetCardStackMock();
         // This will be shuffled
-        var shuffledDeck = GetCardStackMock();
+        var shuffledDeck = CardStackFunctions.GetCardStackMock();
 
-        FillWithCards(unshuffledDeck, 52);
-        FillWithCards(shuffledDeck, 52);
+        CardStackFunctions.FillWithCards(unshuffledDeck, 52);
+        CardStackFunctions.FillWithCards(shuffledDeck, 52);
 
         shuffledDeck.Shuffle(); // Shuffles only one deck
 
@@ -74,22 +72,5 @@ public class DeckUnitTest {
         Assert.That(notEqualCount, Is.GreaterThan(30));
     }
 
-    // FUNCTIONS:
 
-    private CardStack GetCardStackMock()     // Generates a stub for CardStack that simulates the deck
-    {
-        var deck = Substitute.For<CardStack>();
-        deck.isGameDeck = true;
-        deck.cards = new List<int>();   // Initialize fake List to simulate putting in and drawing cards
-        return deck;
-    }
-
-    
-    private void FillWithCards(CardStack c, int numCards) // Fills a given CardStack with 52 cards
-    {
-        for (int i = 0; i < numCards; i++)
-        {
-            c.push(i);   // Add 52 cards, all in sequence, into the both
-        }
-    }
 }
