dfc5724bbb00afb8e83f55fb7c4d5ce421d0a854
Kalyn Williams <43970775+Kaswilli@users.noreply.github.com>
2018-11-17 16:25:36 -0800

    Chance swap

--- GameOfCards/Assets/Tests/ChanceUnitTest.cs
+++ GameOfCards/Assets/Tests/ChanceUnitTest.cs
@@ -9,7 +9,8 @@ using System;
 public class Chance {
 
     [Test]
-    public void test_a_chance_setup_created() { //Checks that players have the same amount of cards in there hand. 
+    public void test_a_chance_setup_created()//Checks that players have the same amount of cards in there hand. 
+    { 
         
         var player = GetCardStackMock();
         var opponent = GetCardStackMock();
@@ -19,18 +20,50 @@ public class Chance {
         Assert.That(opponent.CardCount, Is.EqualTo(3));
 
     }
+    /*
+    void test_b_chance_is_swapped()//Checks that cards are swapped();
+    {
+        var chanceGame = GetChanceMock();
+        for()
+        {
+        //add card value
+        }
 
-    //void test_b_chance_
+        for()
+        {
+        //add swapped value
+        }
+
+        compare the swapped value and unswapped value to see if changed
+    }
+    */
+
+    void test_c_is_winner()//
+    {
+        var chanceGame = GetCardStackMock();
+
+    }
 
 
     private CardStack GetCardStackMock()
     {
         var deck = Substitute.For<CardStack>();
-        deck = new CardStack();
         deck.isGameDeck = true;
         deck.cards = new List<int>();   // Initialize fake List to simulate putting in and drawing cards
         return deck;
     }
+
+
+    private void GetChanceMock()
+    {
+        var chance = Substitute.For<Chance>();
+        var player = GetCardStackMock();
+        var dealer = GetCardStackMock();
+
+        FillWithCards(player, 3);
+        FillWithCards(dealer, 3);
+    }
+
 
     private void  FillWithCards(CardStack c,  int numCards)
     {
