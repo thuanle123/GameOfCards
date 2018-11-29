7fb92788368f7c02b34e02bd11733f93dd8ec17d
Kalyn Williams <43970775+Kaswilli@users.noreply.github.com>
2018-11-29 10:32:57 -0800

    Updated test with draw and winner all working

--- GameOfCards/Assets/Tests/ChanceUnitTest.cs
+++ GameOfCards/Assets/Tests/ChanceUnitTest.cs
@@ -29,7 +29,7 @@ public class ChanceUnitTest {
     }
 
     [Test]
-    public void test_c_player_hand_loses() //Make sure the card value are being properly added together.
+    public void test_c_player_hand_loses() //Make sure the card value are being properly added together to determine a loser.
     {
         var player = CardStackFunctions.GetCardStackMock();
         var dealer = CardStackFunctions.GetCardStackMock();
@@ -46,6 +46,47 @@ public class ChanceUnitTest {
 
         Assert.That(player.ChanceSumValue(), Is.LessThan(dealer.ChanceSumValue())); // Assert  that player will lose
         Assert.That(player.ChanceHandValue(), Is.LessThan(dealer.ChanceHandValue()));
+    }
+
+    [Test]
+    public void test_d_player_hand_wins() //Makes sure the card values are being properly added together to determine a winner
+    {
+        var player = CardStackFunctions.GetCardStackMock();
+        var dealer = CardStackFunctions.GetCardStackMock();
+
+        //Player has all face cards
+        player.push(10);
+        player.push(11);
+        player.push(12);
+
+        //Dealer has score of 3
+        dealer.push(0);
+        dealer.push(13);
+        dealer.push(26);
+
+        Assert.That(player.ChanceSumValue(), Is.GreaterThan(dealer.ChanceSumValue())); //Assert that player will win
+        Assert.That(player.ChanceHandValue(), Is.GreaterThan(dealer.ChanceHandValue()));
+
+    }
+
+    [Test]
+    public void test_e_player_dealer_draw() //Makes sure the card value are being added together to determine a draw
+    {
+        var player = CardStackFunctions.GetCardStackMock();
+        var dealer = CardStackFunctions.GetCardStackMock();
+
+        player.push(10);
+        player.push(11);
+        player.push(12);
+
+        dealer.push(23);
+        dealer.push(24);
+        dealer.push(25);
+
+        Assert.That(player.ChanceSumValue(), Is.EqualTo(dealer.ChanceSumValue())); //Assert that player and dealer will draw
+        Assert.That(player.ChanceHandValue(), Is.EqualTo(dealer.ChanceHandValue()));
+
+
     }
   
     private Chance GetChanceMock()
